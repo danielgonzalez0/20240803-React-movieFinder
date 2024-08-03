e73ed97fb98e3d751913e179f777a90af7f2a5dd
Daniel Gonzalez <daniel.gonzalez.02@gmail.com>
2024-08-03 12:30:21 +0200

    feat: end part 1 app creation and solve parse babel import issue

---
+++ app/page.jsx
@@ -0,0 +1,12 @@
+"use client"
+
+export default function Home() {
+  return (
+    <div className="flex flex-col gap-4 py-8 maw-w-4xl m-auto px-4">
+      <header>
+        <h1 className="text-4xl font-bold text-center">MovieFinder</h1>
+      </header>
+      <main></main>
+    </div>
+  );
+}
